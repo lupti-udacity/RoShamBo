f17c05c97a81176c28db236953d6a07f5c5eae23
lupti-udacity <lupti-udacity@gmail.com>
2015-05-25 10:46:07 -0700

    Initial Commit

---
+++ My RoshamboTests/My_RoshamboTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_RoshamboTests.swift
+//  My RoshamboTests
+//
+//  Created by Lupti on 5/25/15.
+//  Copyright (c) 2015 lupti. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class My_RoshamboTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
